1bd7951f070125c7387b02804e627ee206048b07
FZl47 <wwwfazel88@gmail.com>
2022-08-17 23:06:12 +0430

    Fix bug tables not create

--- SportClub/models.py
+++ SportClub/models.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-_db_path = 'database.sqlite3'
+_db_path = 'db.sqlite'
 
 def create_connection():
     conn = sqlite3.connect(_db_path)
@@ -16,57 +16,55 @@ def close_connection(conn):
 
 _LIST_FUNC_SQL_COMMANDS = []
 def sql_command(func):
+    _LIST_FUNC_SQL_COMMANDS.append(func)
     def wrapper(*args):
-        _LIST_FUNC_SQL_COMMANDS.append(func)
         return func(*args)
     return wrapper
 
 @sql_command
-def create_table_info(conn):
+def create_table_info():
     sql = """
         CREATE TABLE IF NOT EXISTS info (
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             about_us TEXT,
             emails TEXT,
             phones TEXT
-        )
+        );
     """
     return sql
 
 
 @sql_command
-def create_table_time_available(conn):
+def create_table_time_available():
     sql = """
-        CREATE TABLE IF NOT EXISTS time_available(
+        CREATE TABLE IF NOT EXISTS time_available (
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             time_start time NOT NULL,
-            time_end time NOT NULL,
-        )
+            time_end time NOT NULL
+        );
     """
     return sql
 
 
-
+# Get Data Records
 def get_data_info():
     sql = """
-        SELECT * FROM info
-    """ 
+        SELECT * FROM info;
+    """
     conn = create_connection()
     curs = conn.cursor()
-    data = curs.execute(sql)
-    data = data.fetchone() 
+    data = curs.execute(sql).fetchone()
     close_connection(conn)
     return data
 
-get_data_info()
 
 
 
 def _migrate():
     conn = create_connection()
+    cursor = conn.cursor()
     for handler_sql in _LIST_FUNC_SQL_COMMANDS:
         sql = handler_sql()
-        cursor = conn.cursor()
         cursor.execute(sql)
     conn.commit()
     close_connection(conn)
@@ -74,13 +72,4 @@ def _migrate():
 
 
 
-
-def test():
-    sql = """
-        SELECT * FROM info
-    """
-    conn = create_connection()
-    curs = conn.cursor()
-    x = curs.execute(sql).fetchone()
-    print(x)
 
