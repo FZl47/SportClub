237bb25f5b73830510fbdb50c81579ea8436e7e7
FZl47 <wwwfazel88@gmail.com>
2022-08-19 22:25:34 +0430

    Add weekdays conf in config file

--- SportClub/config.py
+++ SportClub/config.py
@@ -1,3 +1,16 @@
 
 DEBUG = True
 STATIC_URL = '/static/'
+
+
+# Note! The order should be like thi
+
+WEEKDAYS = {
+    1 : 'شنبه',
+    2 : 'یکشنبه',
+    3 : 'دوشنبه',
+    4 : 'سه شنبه',
+    5 : 'چهارشنبه',
+    6 : 'پنجشنبه',
+    7 : 'جمعه',
+}
